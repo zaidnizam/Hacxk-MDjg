1afcb147999111e9223a06bd4149ec716caf00ad
zaid <159690637+hacxk@users.noreply.github.com>
2024-06-06 18:25:54 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
 const express = require('express');
 const routes = require('./Utils/Main');
 const { startHacxkMd } = require('./Utils/Bot');
-// require('./Config');
+const http = require('http'); // Require http module for creating the server
+const socketIo = require('socket.io');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.get("/", (req, res) => {
+  res.sendFile("./Utils/FrontEnd/page.html", {
+    root: __dirname,
+  });
+});
+
 app.use(express.json());
 app.use('/api', routes);
 
-app.listen(PORT, () => {
-  startHacxkMd()
+app.get("/scan", (req, res) => {
+  res.sendFile("./Utils/FrontEnd/index.html", {
+    root: __dirname,
+  });
+});
+
+// Create an HTTP server using Express app
+const server = http.createServer(app);
+// Attach socket.io to the HTTP server
+const io = socketIo(server);
+
+
+// Start the server
+server.listen(PORT, () => {
+  startHacxkMd(io, app)
   console.log(`Server is running on port ${PORT}`);
 });
